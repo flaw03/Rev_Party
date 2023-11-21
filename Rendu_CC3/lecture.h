eff51e4a9311f2c901df32d4fe4b49df53b8bbaa
flaw03 <100543813+flaw03@users.noreply.github.com>
2023-11-22 00:12:18 +0100

    kick des fonction inutil dans le .h

--- Rendu_CC3/lecture.h
+++ Rendu_CC3/lecture.h
@@ -12,28 +12,7 @@
 /** \defgroup Constructors Contructors and destructors of the TAD.
  @{
  */
-/** Enlève les sauts de ligne d'une chaine de caractère
-    @param str la chaine à modifier
-    @return la chaine modiée
-*/
-void removeNewline(char* str);
 
-/** Selon un tableau de votes renvoie l'indice du vote.
-  @param votes le tableau de votes
-  @param taille la taille du tableau
-  @return l'indice
-
-*/
-
-int getIndiceVote(int* votes, int taille);
-/**Renvoie le candidat associé au nombre nb
-  @preconditions : nb< taille lstCand
-  @param nb , un nombre
-  @param lstCand la liste de candidats
-  @return le candidat associé au numéro nb
-
-*/
-Candidat* candidatAssocie(int nb, ListCand* lstCand);
 /**Fonction créant une liste d'électeurs selon un fichier.csv
 @param lstElect la liste d'électeurs à remplir (vide)
 @param lstCand la liste de Candidats à associer aux electeurs
@@ -52,26 +31,8 @@ ListElect* getElecteur(ListElect* lstElect, ListCand* lstCand, char* fichier, in
 
 ListCand* getCandidat(ListCand* lstCand, char* fichier);
 
-/** Fonction renvoyant l'indice colonne d'un fichier.csv de chaque candidat présent dans la liste
-@param candidats Les candidats présent
-@param fichier Le fichier.csv à analyser
-@param tab le tableau vide qui contiendra les indices des candidats
-
-@return le tableau contenant les indices des candidats
 
 
-*/
-
-int* indice(ListCand* candidats, char* fichier, int* tab);
-/** Fonction renvoyant l'indice colonne d'un candidat présent dans un fichier.csv
-    Evite de faire appel à une liste
-
-    @param candidat Le candidat à rechercher
-    @param fichier le fichier.csv à  analyser
-
-    @return l'indice colonne du candidat dans le fichier.csv
-
-*/
 int getIndice(Candidat* candidats, char* fichier);
 /** Fonction permettant de créer une liste d'electurs ayant voté pour 2 candidats différents en choisissant celui qu'ils ont préféré.
 @param lstElect liste vide
@@ -84,7 +45,7 @@ int getIndice(Candidat* candidats, char* fichier);
 */
 
 ListElect* electeurs2ndTours2Candidats(ListElect* lstElect, Candidat* c1, Candidat* c2, char* fichier);
-/**
+
 /** Fonction permettant de créer une liste d'electurs ayant voté pour un nombre de candidats différents en choisissant celui qu'ils ont préféré.
 @param lstElect liste vide
 @param lstCand la liste des candidats
