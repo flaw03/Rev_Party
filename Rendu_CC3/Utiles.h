f87424f3c9009ee19b30b570e09b6ec84059be2f
Adridridrian <114427520+Adridridrian@users.noreply.github.com>
2023-11-23 23:59:43 +0100

    Update Utiles.h

--- Rendu_CC3/Utiles.h
+++ Rendu_CC3/Utiles.h
@@ -1,9 +1,32 @@
 #ifndef LISTESUTILES_H
 #define LISTESUTILES_H
 
+
+/**
+ * @file Utiles.h
+ *	@defgroup defgroup Constructors Contructors and destructors of the TAD.
+ * @{
+ */
+
+
+/**
+ * @brief Opaque definition of the ListCand abstract data type.
+ */
 typedef struct s_List_Cand ListCand;
+
+/**
+ * @brief Opaque definition of the ListElect abstract data type.
+ */
 typedef struct s_List_Electeur ListElect;
+
+/**
+ * @brief Opaque definition of the Candidat abstract data type.
+ */
 typedef struct s_Candidat Candidat;
+
+/**
+ * @brief Opaque definition of the Electeur abstract data type.
+ */
 typedef struct s_Electeur Electeur;
 
 struct s_Candidat {
@@ -34,26 +57,26 @@ struct s_List_Cand {
 };
 
 
-
-/*-----------------------------------------------------------------*/
-
-/** \defgroup Constructors Contructors and destructors of the TAD.
- @{
+/** 
+ * @brief Creates a new voter with the specified information.
+ *
+ * @par Profile
+ * @parblock
+ *  create_electeur : \f$\rightarrow\f$ Electeur*.
+ * @endparblock
+ * @param nom Last name of the voter.
+ * @param prenom First name of the voter.
+ * @param candidat Pointer to the candidate chosen by the voter.
+ * @return A pointer to the newly created voter.
  */
-
-/** Creates a new voter with the specified information.
-    @param nom Last name of the voter.
-    @param prenom First name of the voter.
-    @param candidat Pointer to the candidate chosen by the voter.
-    @return A pointer to the newly created voter.
-*/
 Electeur* create_electeur(const char* nom, const char* prenom, Candidat* candidat);
-/** Creates and initializes an empty list of voters.
+
+/** @brief Creates and initializes an empty list of voters.
     @return A pointer to the newly created list of voters.
 */
 ListElect* listElect_create(void);
 
-/** Adds an electeur to the list of electeurs.
+/** @brief Adds an electeur to the list of electeurs.
     @param l The list of electeurs to modify.
     @param electehr Pointer to the electeur to be added.
     @return The modified list of electerrs.
@@ -61,12 +84,12 @@ ListElect* listElect_create(void);
 ListElect* list_add_Electeur(ListElect* l, Electeur* electeur);
 
 
-/** Deletes the entire list of electeurs and frees allocated memory.
+/** @brief Deletes the entire list of electeurs and frees allocated memory.
     @param l The list of electeurs to be deleted.
 */
 void listElect_delete(ListElect* l);
 
-/** Creates a new candidate with the specified information.
+/** @brief Creates a new candidate with the specified information.
     @param nom Last name of the candidate.
     @param prenom First name of the candidate.
     @param age Age of the candidate.
@@ -74,27 +97,26 @@ void listElect_delete(ListElect* l);
 */
 Candidat* create_candidat(const char* nom, const char* prenom, int age);
 
-/** Creates and initializes an empty list of candidates.
+/** @brief Creates and initializes an empty list of candidates.
     @return A pointer to the newly created list of candidates.
 */
 ListCand* listCand_create(void);
 
-/** Adds a candidate to the list of candidates.
+/** @brief Adds a candidate to the list of candidates.
     @param l The list of candidates to modify.
     @param candidat Pointer to the candidate to be added.
     @return The modified list of candidates.
 */
 ListCand* listCand_add(ListCand* l, Candidat* candidat);
 
-/** Deletes the entire list of candidates and frees allocated memory.
+/** @brief Deletes the entire list of candidates and frees allocated memory.
     @param l The list of candidates to be deleted.
 */
 void listCand_delete(ListCand* l);
 
-/**Displays all the informations of each candidate in the list.
+/** @brief Displays all the informations of each candidate in the list.
     @param l The list of candidates to be displayed.
 
 */
-
 void printList(ListCand* l);
 #endif
