3e44f2ea0ceee6c700ac49e282c5e727e800b7e3
YoannSME <115040784+YoannSME@users.noreply.github.com>
2023-12-04 19:49:07 +0100

    Update lecture.h

--- Rendu_CC4/fichiers.h/lecture.h
+++ Rendu_CC4/fichiers.h/lecture.h
@@ -18,30 +18,7 @@
 */
 void removeNewline(char* str);
 
-/** Selon un tableau de votes renvoie l'indice du vote.
-  @param votes le tableau de votes
-  @param taille la taille du tableau
-  @return l'indice
 
-*/
-
-int getIndiceVote(int* votes, int taille);
-/**Renvoie le candidat associé au nombre nb
-  @preconditions : nb< taille lstCand
-  @param nb , un nombre
-  @param lstCand la liste de candidats
-  @return le candidat associé au numéro nb
-
-*/
-Candidat* candidatAssocie(int nb, ListCand* lstCand);
-/**Fonction créant une liste d'électeurs selon un fichier.csv
-@param lstElect la liste d'électeurs à remplir (vide)
-@param lstCand la liste de Candidats à associer aux electeurs
-@param fichier l'accès aux données
-@param tab Le tableau de votes des electeurs
-@return une liste d'electeurs
-
-*/
 ListElect* getElecteur(ListElect* lstElect, ListCand* lstCand, char* fichier, int* tab);
 
 /**Fonction créant une liste de candidats à partir d'un fichier.csv
@@ -62,16 +39,6 @@ ListCand* getCandidat(ListCand* lstCand, char* fichier);
 
 */
 
-int* indice(ListCand* candidats, char* fichier, int* tab);
-/** Fonction renvoyant l'indice colonne d'un candidat présent dans un fichier.csv
-    Evite de faire appel à une liste
-
-    @param candidat Le candidat à rechercher
-    @param fichier le fichier.csv à  analyser
-
-    @return l'indice colonne du candidat dans le fichier.csv
-
-*/
 int getIndice(Candidat* candidats, char* fichier);
 /** Fonction permettant de créer une liste d'electurs ayant voté pour 2 candidats différents en choisissant celui qu'ils ont préféré.
 @param lstElect liste vide
@@ -84,20 +51,12 @@ int getIndice(Candidat* candidats, char* fichier);
 */
 
 ListElect* electeurs2ndTours2Candidats(ListElect* lstElect, Candidat* c1, Candidat* c2, char* fichier,FILE* logfile);
-/** Fonction permettant de créer une liste d'electurs ayant voté pour un nombre de candidats différents en choisissant celui qu'ils ont préféré.
-@param lstElect liste vide
-@param lstCand la liste des candidats
-@param fichier le fichier contenant les votes et les informations des electeurs
-@param tab Le tableau contenant les votes de chaque electeur
-@return Une liste d'électeurs
 
-
+/**Fonction permettant d'initialiser les votes d'une liste de candidat suivant les votes d'une liste d'électeurs
+@param lstCand Liste de Candidat
+@param lstElect Liste d'électeurs
+@param fichier Fichier contenant les votes des électeurs
 */
-ListElect* getElecteur2ndTour(ListElect* lstElect, ListCand* lstCand, char* fichier, int* tab);
-
-
-void createTableau(int taille,ListCand* lstCand);
-
 void initJugement(ListCand* lstCand,ListElect* lstElect,char* fichier);
 
 #endif
