233b1fa76653622c7f8103bcf3422d67b4bd60d1
YoannSME <115040784+YoannSME@users.noreply.github.com>
2023-12-08 03:16:13 +0100

    Update scrutin.c
    Gestion fichier non conforme

--- Rendu_CC4/main_module/scrutin.c
+++ Rendu_CC4/main_module/scrutin.c
@@ -62,6 +62,7 @@ int main(int argc, char** argv) {
         switch (opt) {
             case 'i':
                 filename= optarg;
+                verifFichier(filename);
                 nbrElecteur = lireBallot(filename,&matriceDuel);
                 optionBalot = true;
                 break;
