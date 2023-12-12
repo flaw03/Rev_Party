b03575bc24e7144fe61bd2239dec084571c190e8
YoannSME <115040784+YoannSME@users.noreply.github.com>
2023-12-12 17:24:04 +0100

    Trad uninominales.h

--- Code/scrutin_methods/uninominales.h
+++ Code/scrutin_methods/uninominales.h
@@ -3,152 +3,97 @@
 
 /**
  *  @file Uninominales.h
- *	@defgroup defgroup Constructors Contructors and destructors of the TAD.
+ *	@defgroup defgroup Constructeurs et destructeurs du TAD (Type Abstrait de Données).
  *  @{
  */
 
-
-
-
 /**
- * @brief Opaque definition of the Resultat abstract data type.
+ * @brief Définition opaque du type de données abstrait Resultat.
  */
 typedef struct s_Resultat Resultat;
 
 /**
- * @brief Function initializing a structure Resultat
+ * @brief Fonction de vote uninominal à un tour.
  * 
- * @par Profile
- * @parblock
- *	affecter_resultat : \f$\rightarrow \f$ Resultat.
- * @endparblock
- * @param candidat The winning candidate
- * @param nb the number of votes
- *
- * @return a structure Resultat
- * 
- */
-Resultat affecter_resultat(Candidat* candidat, double nb);
-
-/**
- * @brief Function initializing the votes of candidates following a list of voters
- * 
- * @par Profile
- * @parblock
- *	initVote : \f$\rightarrow \f$ ListCand*.
- * @endparblock
- * @param lstElect a list of voters
- * @param lstCand a list of candidates
- * @param fichier a log file allowing you to store the algorithm calculations
- *
- * @return A list of candidates with their incremented votes
- * 
- */
-ListCand* initVote(ListElect* lstElect, ListCand* lstCand, FILE* fichier);
-
-/**
- * @brief First-past-the-post voting function
- * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	uninominal_un_tour : \f$\rightarrow \f$ Resultat.
  * @endparblock
- * @param lstElect a list of voters
- * @param lstCand a list of candidates
- * @param fichier a log file allowing you to store the algorithm calculations
- * @return a Resultat structure containing the winning candidate and their number of votes
- *
+ * @param lstElect Une liste d'électeurs.
+ * @param lstCand Une liste de candidats.
+ * @param fichier Un fichier journal permettant de stocker les calculs de l'algorithme.
+ * @return Une structure Resultat contenant le candidat gagnant et son nombre de votes.
  */
 Resultat uninominal_un_tour(ListElect* lstElect, ListCand* lstCand, FILE* fichier);
 
-/**
- * @brief Function allowing you to initialize an array of Candidates to respect the type of a function when you have a majority winner
- * 
- * @par Profile
- * @parblock
- *	createRetour : \f$\rightarrow \f$ Candidat**.
- * @endparblock
- * @param tab an empty candidate array
- * @param candidat The candidate who won by a majority
- * @return an array containing a candidate who won by a majority in the two-round single-member vote
- * 
- */
-Candidat** createRetour(Candidat** tab, Candidat* candidat);
 
 /**
- * @brief Function allowing you to retrieve from an array the two candidates with the most votes
+ * @brief Fonction permettant de récupérer à partir d'un tableau les deux candidats ayant le plus de votes.
  * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	finalistes_uninominal_deux_tours : \f$\rightarrow \f$ Candidat**.
  * @endparblock
- * @param lstElect a list of voters
- * @param lstCand a list of candidates
- * @param fichier a log file allowing you to store the algorithm calculations
- * @return an array containing the two candidates with the most votes OR an array containing the candidate having had the absolute majority of votes
- * 
+ * @param lstElect Une liste d'électeurs.
+ * @param lstCand Une liste de candidats.
+ * @param fichier Un fichier journal permettant de stocker les calculs de l'algorithme.
+ * @return Un tableau contenant les deux candidats avec le plus de votes OU un tableau contenant le candidat ayant eu la majorité absolue de votes.
  */
 Candidat** finalistes_uninominal_deux_tours(ListElect* lstElect, ListCand* lstCand, FILE* fichier);
 
 /**
- * @brief Function allowing you to retrieve the candidate with the most votes between the two finalist candidates
+ * @brief Fonction permettant de récupérer le candidat avec le plus de votes entre les deux candidats finalistes.
  * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	uninominal_2nd_Tours : \f$\rightarrow \f$ Resultat.
  * @endparblock
- * @param lstElecteurs a list of voters
- * @param finalistes an array of finalists
- * @param fichier a log file allowing you to store the algorithm calculations
- * @return A Resultat structure containing the winning candidate and their votes
- * 
+ * @param lstElecteurs Une liste d'électeurs.
+ * @param finalistes Un tableau de finalistes.
+ * @param fichier Un fichier journal permettant de stocker les calculs de l'algorithme.
+ * @return Une structure Resultat contenant le candidat gagnant et ses votes.
  */
 Resultat uninominal_2nd_Tours(ListElect* lstElecteurs, Candidat** finalistes, FILE* logfile);
 
 /**
- * @brief Test function to create voting electors for known candidates randomly.
+ * @brief Fonction de test pour créer des électeurs votants pour des candidats connus de manière aléatoire.
  * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	createTest : \f$\rightarrow \f$ ListElect*.
  * @endparblock
- * @param lstElect an empty list of voters
- * @param lstCand a list of candidates
- * @param nbTest a defined number of tests
- * @return a list containing nbTest voters
- * 
+ * @param lstElect Une liste vide d'électeurs.
+ * @param lstCand Une liste de candidats.
+ * @param nbTest Un nombre défini de tests.
+ * @return Une liste contenant nbTest électeurs.
  */
 ListElect* createTest(ListElect* lstElect, ListCand* lstCand, int nbTest);
 
 /**
- * @brief First-past-the-post vote formatting function
+ * @brief Fonction de mise en forme du vote uninominal à un tour.
  * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	unTour : \f$\rightarrow \f$ void.
  * @endparblock
- * @param fichier file containing data
- * @param fichier a log file allowing you to store the algorithm calculations
- * 
+ * @param fichier Fichier contenant les données.
+ * @param fichier Un fichier journal permettant de stocker les calculs de l'algorithme.
  */
 void unTour(char* fichier, FILE* logfile);
 
 /**
- * @brief Two-round first-past-the-post vote formatting function
+ * @brief Fonction de mise en forme du vote uninominal à deux tours.
  * 
- * @par Profile
+ * @par Profil
  * @parblock
  *	deuxTours : \f$\rightarrow \f$ void.
  * @endparblock
- * @param a log file allowing you to store the algorithm calculations
- * @param fichier fichier file containing data
- * 
+ * @param Un fichier journal permettant de stocker les calculs de l'algorithme.
+ * @param fichier Fichier contenant les données.
  */
 void deuxTours(FILE* logfile, char* fichier);
 
 /** @} */
-
-
 
 /** @} */
 
